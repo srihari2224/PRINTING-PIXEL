7d80b03adc28e874c6c3cc627bdd3bbabfa8e6c1
m srihari <msrihari2224@gmail.com>
2026-02-26 03:58:13 +0530

    otp validation increase

--- src/services/otp.service.js
+++ src/services/otp.service.js
@@ -4,7 +4,7 @@ const generateOTP = require("../utils/generateOTP")
 exports.createOTP = async ({ uploadId, kioskId }) => {
   const otp = generateOTP()
 
-  const expiresAt = new Date(Date.now() + 10 * 60 * 1000) // 10 min
+  const expiresAt = new Date(Date.now() + 3 * 24 * 60 * 60 * 1000) // 3 days
 
   await OTP.create({
     otp,
